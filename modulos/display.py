a0418a660d799874cf2581422d1163c018dde64e
Carlos <carlosmartiguerre@gmail.com>
2021-05-04 19:58:39 +0200

    Actualizo display con el nivel de muestra

--- modulos/display.py
+++ modulos/display.py
@@ -97,16 +97,17 @@ def display(texto,opciones):
 
         elif key == "Key.enter":
             enterKey = str(posicion)
+            
 
         else:
             pass
-
+        
         l.stop()
-
 
     global enterKey
     global opciones_default
     enterKey = None
+    clave = None
 
     opciones[0] = "\x1b[31m - "+opciones[0]+"\x1b[0m "
 
@@ -125,17 +126,23 @@ def display(texto,opciones):
         with Listener(on_press=check_key) as l:
             l.join()
 
+        if enterKey != None:
+
+            for i in range(len(opciones)):
+                if "\x1b[31m" in opciones[i]:
+                    clave = i
+                    
+        if clave != None:
+            break
+
         print(("\033[F") * 27)
         pantalla(texto,opciones)
 
-        if enterKey != None:
-            break
-
-
-    return enterKey
+    return clave
     
 
-    
+
+  
 
 
 
